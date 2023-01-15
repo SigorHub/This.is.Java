685eea402bd78cc3a040d424758dfc509d01fdf6
SigorShop <SigorShop@SigorShop>
2023-01-16 00:32:39 +0900

    Chap01

---
+++ This.is.Java/src/Chap01/Hello.java
@@ -0,0 +1,10 @@
+package Chap01;
+
+public class Hello {
+	public static void main(String[] args) {						//프로그램 메인 스레드 실행
+		/*														//주석문
+		 작성일 : 2023.01.14
+		 */
+		System.out.println("Hell, welcome to the Java World !");	//실행문 콘솔에 문자열 출력
+	}															//메인 스레드 종료
+}																//클레스 종료
